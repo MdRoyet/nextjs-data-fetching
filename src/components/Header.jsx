b55d47265cd2209f0ef186fd4775d0aa5a2a81d6
Md Royet <rroyet8@gmail.com>
2026-04-20 23:18:33 +0600

    Pages created using daisyUI & Edit it

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+const Header = () => {
+  const links = (
+    <>
+      <li>
+        <a>Item 12</a>
+      </li>
+      <li>
+        <a>Item 13</a>
+      </li>
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden"
+            ></div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
