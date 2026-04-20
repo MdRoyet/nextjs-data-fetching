a58f9ce3529aec3e605f53e82bdff498fc9ddfe0
Md Royet <rroyet8@gmail.com>
2026-04-21 01:11:12 +0600

    Json Server creating

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,5 +1,8 @@
 const getPost = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts");
+  }
   return res.json();
 };
 
